10ca38285f9ae2bcd0493d78a4d157a7efe66af0
idwer@localhost <idwer@localhost>
2021-07-27 15:04:55 +0200

    user interface: add code behind buttons

--- Qalculator/mainwindow.cpp
+++ Qalculator/mainwindow.cpp
@@ -13,3 +13,94 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::on_btnZero_clicked()
+{
+    ui->lineEdit_LeftHand->setText(ui->lineEdit_LeftHand->text() + ui->btnZero->text());
+}
+
+void MainWindow::on_btnOne_clicked()
+{
+    ui->lineEdit_LeftHand->setText(ui->lineEdit_LeftHand->text() + ui->btnOne->text());
+}
+
+void MainWindow::on_btnTwo_clicked()
+{
+    ui->lineEdit_LeftHand->setText(ui->lineEdit_LeftHand->text() + ui->btnTwo->text());
+}
+
+void MainWindow::on_btnThree_clicked()
+{
+    ui->lineEdit_LeftHand->setText(ui->lineEdit_LeftHand->text() + ui->btnThree->text());
+}
+
+void MainWindow::on_btnFour_clicked()
+{
+    ui->lineEdit_LeftHand->setText(ui->lineEdit_LeftHand->text() + ui->btnFour->text());
+}
+
+void MainWindow::on_btnFive_clicked()
+{
+    ui->lineEdit_LeftHand->setText(ui->lineEdit_LeftHand->text() + ui->btnFive->text());
+}
+
+void MainWindow::on_btnSix_clicked()
+{
+    ui->lineEdit_LeftHand->setText(ui->lineEdit_LeftHand->text() + ui->btnSix->text());
+}
+
+void MainWindow::on_btnSeven_clicked()
+{
+    ui->lineEdit_LeftHand->setText(ui->lineEdit_LeftHand->text() + ui->btnSeven->text());
+}
+
+void MainWindow::on_btnEight_clicked()
+{
+    ui->lineEdit_LeftHand->setText(ui->lineEdit_LeftHand->text() + ui->btnEight->text());
+}
+
+void MainWindow::on_btnNine_clicked()
+{
+    ui->lineEdit_LeftHand->setText(ui->lineEdit_LeftHand->text() + ui->btnNine->text());
+}
+
+void MainWindow::on_btnSeparator_clicked()
+{
+    ui->lineEdit_LeftHand->setText(ui->lineEdit_LeftHand->text() + ui->btnSeparator->text());
+}
+
+void MainWindow::on_btnAdd_clicked()
+{
+    int64_t left = ui->lineEdit_LeftHand->text().toDouble();
+    int64_t right = ui->lineEdit_RightHand->text().toDouble();
+
+    ui->lineEdit->setText(QString::number(ArithmeticOperation::Add(left, right)));
+}
+
+void MainWindow::on_btnSubtract_clicked()
+{
+    int64_t left = ui->lineEdit_LeftHand->text().toDouble();
+    int64_t right = ui->lineEdit_RightHand->text().toDouble();
+
+    ui->lineEdit->setText(QString::number(ArithmeticOperation::Subtract(left, right)));
+}
+
+void MainWindow::on_btnDivide_clicked()
+{
+    int64_t left = ui->lineEdit_LeftHand->text().toDouble();
+    int64_t right = ui->lineEdit_RightHand->text().toDouble();
+
+    ui->lineEdit->setText(QString::number(ArithmeticOperation::Divide(left, right)));
+}
+
+void MainWindow::on_btnMultiply_clicked()
+{
+    int64_t left = ui->lineEdit_LeftHand->text().toDouble();
+    int64_t right = ui->lineEdit_RightHand->text().toDouble();
+
+    ui->lineEdit->setText(QString::number(ArithmeticOperation::Multiply(left, right)));
+}
+
+void MainWindow::on_btnResult_clicked()
+{
+//    ui->lineEdit_LeftHand->setText(ui->lineEdit_LeftHand->text() + ui->btnResult->text());
+}
--- Qalculator/mainwindow.h
+++ Qalculator/mainwindow.h
@@ -17,5 +17,24 @@ public:
 
 private:
     Ui::MainWindow *ui;
+
+private slots:
+    void on_btnSeparator_clicked();
+    void on_btnZero_clicked();
+    void on_btnOne_clicked();
+    void on_btnTwo_clicked();
+    void on_btnThree_clicked();
+    void on_btnFour_clicked();
+    void on_btnFive_clicked();
+    void on_btnSix_clicked();
+    void on_btnSeven_clicked();
+    void on_btnEight_clicked();
+    void on_btnNine_clicked();
+
+    void on_btnAdd_clicked();
+    void on_btnSubtract_clicked();
+    void on_btnDivide_clicked();
+    void on_btnMultiply_clicked();
+    void on_btnResult_clicked();
 };
 #endif // MAINWINDOW_H
