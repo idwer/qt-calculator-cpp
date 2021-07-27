3c56c37af1f68c2098ab856bfc127f8ceb351942
idwer@localhost <idwer@localhost>
2021-07-27 15:08:20 +0200

    add class ArithmeticOperation carrying static functions

---
+++ Qalculator/arithmeticoperation.cpp
@@ -0,0 +1,21 @@
+#include "arithmeticoperation.h"
+
+double ArithmeticOperation::Add(float left, float right)
+{
+    return left + right;
+}
+
+double ArithmeticOperation::Subtract(float left, float right)
+{
+    return left - right;
+}
+
+double ArithmeticOperation::Divide(float left, float right)
+{
+    return left / right;
+}
+
+double ArithmeticOperation::Multiply(float left, float right)
+{
+    return left * right;
+}
---
+++ Qalculator/arithmeticoperation.h
@@ -0,0 +1,13 @@
+#ifndef ARITHMETICOPERATION_H
+#define ARITHMETICOPERATION_H
+
+class ArithmeticOperation
+{
+public:
+    static double Add(float left, float right);
+    static double Subtract(float left, float right);
+    static double Divide(float left, float right);
+    static double Multiply(float left, float right);
+};
+
+#endif // ARITHMETICOPERATION_H
